48b9aa442aa1d6f13375c21889ac85ae1ce72815
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-04-16 23:26:59 +0300

    shockolate: play MIDI files

--- games/shockolate/files/patch-src_MusicSrc_MusicDevice.c
+++ games/shockolate/files/patch-src_MusicSrc_MusicDevice.c
@@ -1,20 +1,91 @@
---- src/MusicSrc/MusicDevice.c.orig	2020-01-18 07:05:35.000000000 +0300
-+++ src/MusicSrc/MusicDevice.c	2020-04-16 07:10:01.418420000 +0300
-@@ -1184,7 +1184,7 @@
+--- src/MusicSrc/MusicDevice.c.orig	2020-04-16 20:07:36.000000000 +0300
++++ src/MusicSrc/MusicDevice.c	2020-04-16 23:20:17.954796000 +0300
+@@ -1023,9 +1023,10 @@
+     fluid_settings_t *settings;
+     fluid_synth_t *synth;
+     int sfid;
+-    char fileName[1024] = "res/";
++    char fileName[1024] = "%%MIDIDIR%%";
++    size_t dirlen = strlen (fileName);
+ 
+-    FluidMidiGetOutputName(dev, outputIndex, &fileName[4], 1020);
++    FluidMidiGetOutputName(dev, outputIndex, &fileName[dirlen], 1024 - dirlen);
+     if (strlen(fileName) == 4)
+     {
+         WARN("Failed to locate SoundFont for outputIndex=%d", outputIndex);
+@@ -1184,19 +1185,20 @@
          FindClose(hFind);
      }
  #else
 -    DIR *dirp = opendir("res");
-+    DIR *dirp = opendir("%%DATADIR%%res");
++    DIR *dirp = opendir("%%MIDIDIR%%");
      struct dirent *dp = 0;
-     while (dp = readdir(dirp))
-     {
-@@ -1237,7 +1237,7 @@
+-    while ((dp = readdir(dirp)))
+-    {
+-        char *filename = dp->d_name;
+-        char namelen = strlen(filename);
+-        if (namelen < 4) continue; // ".sf2"
+-        if (strcasecmp(".sf2", (char*)(filename + (namelen - 4)))) continue;
+-        // found one
+-        // INFO("Counting SoundFont file: %s", filename);
+-        ++outputCount;
++    if (dirp != NULL) {
++        while ((dp = readdir(dirp))) {
++            char *filename = dp->d_name;
++            char namelen = strlen(filename);
++            if (namelen < 4) continue; // ".sf2"
++            if (strcasecmp(".sf2", (char*)(filename + (namelen - 4)))) continue;
++            // found one
++            // INFO("Counting SoundFont file: %s", filename);
++            ++outputCount;
++        }
++        closedir(dirp);
+     }
+-    closedir(dirp);
+ #endif
+ 
+     // suppress compiler warnings
+@@ -1237,24 +1239,26 @@
      unsigned int outputCount = 0; // subtract 1 to get index
      // count .sf2 files in res/ subdirectory until we find the one that the user
      //  probably wants
 -    DIR *dirp = opendir("res");
-+    DIR *dirp = opendir("%%DATADIR%%res");
-     struct dirent *dp = 0;
-     while ((outputCount <= outputIndex) &&
-            (dp = readdir(dirp)))
+-    struct dirent *dp = 0;
+-    while ((outputCount <= outputIndex) &&
+-           (dp = readdir(dirp)))
+-    {
+-        char *filename = dp->d_name;
+-        char namelen = strlen(filename);
+-        if (namelen < 4) continue; // ".sf2"
+-        if (strcasecmp(".sf2", (char*)(filename + (namelen - 4)))) continue;
+-        // found one
+-        // INFO("Counting SoundFont file: %s", filename);
+-        ++outputCount;
+-        if (outputCount - 1 != outputIndex) continue;
+-        // found it
+-        strncpy(buffer, filename, bufferSize - 1);
+-        // INFO("Found SoundFont file for outputIndex=%d: %s", outputIndex, filename);
++    DIR *dirp = opendir("%%MIDIDIR%%");
++
++    if (dirp != NULL) {
++        struct dirent *dp = 0;
++        while ((outputCount <= outputIndex) &&
++               (dp = readdir(dirp))) {
++            char *filename = dp->d_name;
++            char namelen = strlen(filename);
++            if (namelen < 4) continue; // ".sf2"
++            if (strcasecmp(".sf2", (char*)(filename + (namelen - 4)))) continue;
++            // found one
++            // INFO("Counting SoundFont file: %s", filename);
++            ++outputCount;
++            if (outputCount - 1 != outputIndex) continue;
++            // found it
++            strncpy(buffer, filename, bufferSize - 1);
++            // INFO("Found SoundFont file for outputIndex=%d: %s", outputIndex, filename);
++        }
++        closedir(dirp);
+     }
+-    closedir(dirp);
+ #endif
+     // put NULL in last position in case we filled up everything else
+     *(buffer + bufferSize - 1) = '\0';
