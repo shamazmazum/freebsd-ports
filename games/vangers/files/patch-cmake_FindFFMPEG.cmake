c2e6b60fdc6c4b780c392f39021dce979076f0a8
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-04-29 14:07:05 +0300

    Fix games/vangers

---
+++ games/vangers/files/patch-cmake_FindFFMPEG.cmake
@@ -0,0 +1,11 @@
+--- cmake/FindFFMPEG.cmake.orig	2023-04-29 11:16:08 UTC
++++ cmake/FindFFMPEG.cmake
+@@ -6,6 +6,8 @@
+ # FFMPEG_LIBRARIES
+ #
+ 
++set(CMAKE_PREFIX_PATH "%%LOCALBASE%%/ffmpeg4;%%LOCALBASE%%/ffmpeg4/libexec")
++
+ FIND_PATH(AVUTIL_INCLUDE_DIR
+ 	NAMES
+ 		avutil.h
