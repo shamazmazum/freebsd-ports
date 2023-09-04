340fd4fe8a290621a0784175270d3151d9cd6dd4
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-09-04 11:29:07 +0300

    math/py-matplotlib: Update to 3.7.2

--- math/py-matplotlib/files/patch-setup.py
+++ math/py-matplotlib/files/patch-setup.py
@@ -4,7 +4,7 @@
  
  setup(  # Finally, pass this all along to setuptools to do the heavy lifting.
      name="matplotlib",
-+    version="3.7.1",
++    version="3.7.2",
      description="Python plotting package",
      author="John D. Hunter, Michael Droettboom",
      author_email="matplotlib-users@python.org",
