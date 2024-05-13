33e916fc086c8322e21363db923041695305ceaf
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-05-13 07:30:01 +0300

    Add games/perimeter

---
+++ games/perimeter/files/patch-Source_XTool_xmath.h
@@ -0,0 +1,11 @@
+--- Source/XTool/xmath.h.orig	2024-05-13 04:03:51 UTC
++++ Source/XTool/xmath.h
+@@ -84,7 +84,7 @@ const int INT_INF = 0x7fffffff;
+ ///////////////////////////////////////////////////////////////////////////////
+ 
+ //Use gamemath instead of stdlib
+-#define XMATH_USE_GAMEMATH
++//#define XMATH_USE_GAMEMATH
+ 
+ namespace xm {
+     //Math functions
