5b0f01542bbfb8910fd2ef6b0390de011d2351f2
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-12-02 20:44:12 +0300

    www/webkit2-gtk3: Update to 2.46.4

--- www/webkit2-gtk3/files/patch-Source_WebKit_UIProcess_Launcher_glib_ProcessLauncherGLib.cpp
+++ www/webkit2-gtk3/files/patch-Source_WebKit_UIProcess_Launcher_glib_ProcessLauncherGLib.cpp
@@ -1,6 +1,6 @@
---- Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp.orig	2024-09-21 16:06:53 UTC
+--- Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp.orig	2024-10-21 09:00:02 UTC
 +++ Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp
-@@ -150,7 +150,7 @@ void ProcessLauncher::launchProcess()
+@@ -160,7 +160,7 @@ void ProcessLauncher::launchProcess()
      }
  
      realExecutablePath = FileSystem::fileSystemRepresentation(executablePath);
@@ -9,7 +9,7 @@
  
  #if ENABLE(DEVELOPER_MODE)
      Vector<CString> prefixArgs;
-@@ -177,7 +177,6 @@ void ProcessLauncher::launchProcess()
+@@ -187,7 +187,6 @@ void ProcessLauncher::launchProcess()
      argv[i++] = const_cast<char*>(realExecutablePath.data());
      argv[i++] = processIdentifier.get();
      argv[i++] = webkitSocket.get();
