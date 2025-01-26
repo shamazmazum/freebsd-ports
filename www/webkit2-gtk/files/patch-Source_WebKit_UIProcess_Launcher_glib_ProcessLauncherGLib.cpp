0bab71951409bfe5ac0723699c45e78b6c436359
Vasily Postnicov <shamaz.mazum@gmail.com>
2025-01-26 20:18:25 +0300

    Add www/webkit2-gtk, remove www/webkit2-gtk(3,4)

--- www/webkit2-gtk/files/patch-Source_WebKit_UIProcess_Launcher_glib_ProcessLauncherGLib.cpp
+++ www/webkit2-gtk/files/patch-Source_WebKit_UIProcess_Launcher_glib_ProcessLauncherGLib.cpp
@@ -1,19 +1,24 @@
---- Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp.orig	2024-10-21 09:00:02 UTC
+--- Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp.orig	2024-09-30 08:15:01 UTC
 +++ Source/WebKit/UIProcess/Launcher/glib/ProcessLauncherGLib.cpp
-@@ -160,7 +160,7 @@ void ProcessLauncher::launchProcess()
+@@ -160,7 +160,11 @@ void ProcessLauncher::launchProcess()
      }
  
      realExecutablePath = FileSystem::fileSystemRepresentation(executablePath);
--    unsigned nargs = 5; // size of the argv array for g_spawn_async()
++#if OS(LINUX)
+     unsigned nargs = 5; // size of the argv array for g_spawn_async()
++#else
 +    unsigned nargs = 4; // size of the argv array for g_spawn_async()
++#endif
  
  #if ENABLE(DEVELOPER_MODE)
      Vector<CString> prefixArgs;
-@@ -187,7 +187,6 @@ void ProcessLauncher::launchProcess()
+@@ -187,7 +191,9 @@ void ProcessLauncher::launchProcess()
      argv[i++] = const_cast<char*>(realExecutablePath.data());
      argv[i++] = processIdentifier.get();
      argv[i++] = webkitSocket.get();
--    argv[i++] = pidSocketString.get();
++#if OS(LINUX)
+     argv[i++] = pidSocketString.get();
++#endif
  #if ENABLE(DEVELOPER_MODE)
      if (configureJSCForTesting)
          argv[i++] = const_cast<char*>("--configure-jsc-for-testing");
