716749f4f5877537870cf7f7c3859f25c12267f9
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-05-19 08:04:58 +0300

    games/yquake2-ref_vk: Link against SDL3

---
+++ games/yquake2-ref_vk/files/patch-src_common_header_ref__api.h
@@ -0,0 +1,11 @@
+--- src/common/header/ref_api.h.orig	2026-05-19 05:26:09 UTC
++++ src/common/header/ref_api.h
+@@ -125,7 +125,7 @@ typedef enum {
+ } ref_restart_t;
+ 
+ // FIXME: bump API_VERSION?
+-#define	API_VERSION		7
++#define	API_VERSION		8
+ #define EXPORT
+ #define IMPORT
+ 
